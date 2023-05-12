be8c82838193f334c1698b1040c63ab0b873694e
tyco1ina <ty_colina22@skynetcomputer.myfiosgateway.com>
2023-05-11 22:44:02 -0400

    added photo upload functionality

--- app/capture.js
+++ app/capture.js
@@ -1,13 +1,22 @@
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Camera, CameraType } from "expo-camera";
-import { useState } from 'react';
+import { shareAsync } from 'expo-sharing'
+import { useState, useRef } from 'react';
 import { useRouter } from 'expo-router';
 import { Icon } from "react-native-elements";
+import * as MediaLibrary from 'expo-media-library'
 import RoundButton from '../components/RoundButton';
+import * as ImagePicker from 'expo-image-picker';
+
 
 export default function App() {
+    let cameraRef = useRef();
     const [type, setType] = useState(CameraType.back);
+    const [capturedImage, setCapturedImage] = useState(null);
     const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    let camera = null;
+
     const router = useRouter()
 
     if (!permission) {
@@ -23,15 +32,53 @@ export default function App() {
         )
     }
 
+    const handleTakePicture = async () => {
+        let options = {
+            quality: 1,
+            base64: true,
+            exif: false
+        };
+
+        let newPhoto = await cameraRef.current.takePictureAsync(options);
+        console.log(newPhoto['uri'])
+        sendPhoto(newPhoto)
+        setCapturedImage(newPhoto['uri'])
+    }
+
+    /**
+     * Function written by ChatGPT to send the photo to the backend
+     */
+    const sendPhoto = async (photo) => {
+        const formData = new FormData();
+        formData.append('file', {
+          uri: photo.uri,
+          type: 'image/jpeg',
+          name: 'photo.jpg'
+        });
+    
+        try {
+          const response = await fetch('https://strikezone-backend-app-e32wvb46ka-uc.a.run.app/captures/post-image', {
+            method: 'POST',
+            body: formData
+          });
+          console.log(response)
+          const responseData = await response.json();
+          console.log(responseData);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+    // React freaks out when the '<' symbol is inside an element, so this is to make sure that doesn't happen
     let backToMenuText = "< Main Menu"
 
     return (
         <SafeAreaView style={styles.container}>
-            <Camera style={{flex: 1}} type={type}>
+            <Camera style={{flex: 1}} type={type} ref={cameraRef}>
                     <Text style={styles.backButton} onPress={() => router.push('/')}>{backToMenuText}</Text>
                     <View style={styles.scorecardRectangle}/>
                     <View style={styles.buttonContainer}>
-                        <TouchableOpacity style={styles.button}>
+                        <TouchableOpacity style={styles.button} onPress={handleTakePicture}>
                             <View style={styles.circle}/>
                         </TouchableOpacity>
                     </View>
