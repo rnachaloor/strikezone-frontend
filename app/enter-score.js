d0914fddc96313480fc73e0e208393e76852da1f
tyco1ina <ty_colina22@skynetcomputer.myfiosgateway.com>
2023-05-16 21:11:13 -0400

    finally completed this component

--- app/enter-score.js
+++ app/enter-score.js
@@ -15,7 +15,7 @@ import Scorecard from '../components/Scorecard';
 
 export default function App() {
 
-    const [symbols, setSymbols] = useState(['X', '9-', '8/', 'X', 'X', '9/', '7-', 'X', '9/', '6/9']);
+    const [symbols, setSymbols] = useState(['', '', '', '', '', '', '', '', '', '4']);
     const [scores, setScores] = useState([]);
     const [frameInputBorderColor, setFrameInputBorderColor] = useState('black');
     const [frameToEdit, setFrameToEdit] = useState(1);
@@ -28,6 +28,18 @@ export default function App() {
 
     let backToMenuText = "< Capture"
     const router = useRouter()
+
+    const handleLeftArrow = () => {
+        if (frameToEdit != 1) {
+            setFrameToEdit(frameToEdit - 1)
+        }
+    }
+
+    const handleRightArrow = () => {
+        if (frameToEdit != 10) {
+            setFrameToEdit(frameToEdit + 1)
+        }
+    }
 
     // List of functions to handle a number key press
     const handle0key = () => {
@@ -75,39 +87,233 @@ export default function App() {
     }
 
     const handleClear = () => {
-        handleScorecardChange(0)
+        let newSymbols = [...symbols]
+        newSymbols[frameToEdit - 1] = ""
+        setSymbols(newSymbols)
     }
 
     // Handle when the frame number input is changed
     const handleFrameInputChange = (value) => {
         setFrameToEdit(value)
+
+        
+    }
+
+    const tenthIsComplete = (tenthFrameSymbol) => {
+        if (tenthFrameSymbol.length == 3) {
+            return true
+        }
+
+        // tenth frame symbol length = 2 
+        if (tenthFrameSymbol.length == 2 && tenthFrameSymbol[0] != "X" && tenthFrameSymbol[1] != "/") {
+            return true
+        } 
+        return false
     }
 
     // Handles when the enter button is pressed
     const handleScorecardChange = (pins) => {
+        let newSymbols = [...symbols]
 
-        setFrameSymbol(symbols[frameToEdit - 1])
+        if (frameToEdit == 10) {
 
-        if (pins === 10) {
-            let currentSymbolsList = symbols
-            currentSymbolsList[frameToEdit - 1] = "X"
-            setSymbols(currentSymbolsList)
-            setFrameToEdit(frameToEdit + 1)
+            if (tenthIsComplete(newSymbols[frameToEdit - 1])) {
+                
+            } else if (newSymbols[frameToEdit - 1].length == 0) {
+                if (pins == 0) {
+                    newSymbols[frameToEdit - 1] = "-"
+                    setSymbols(newSymbols)
+                } else if (pins == 10) {
+                    newSymbols[frameToEdit - 1] = "X"
+                    setSymbols(newSymbols)
+                } else {
+                    newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + pins + ""
+                    setSymbols(newSymbols)
+                }
+            } else if (newSymbols[frameToEdit - 1].length == 1) {
+                // If the first frame is a strike, just add the number of the pinfall unless strike
+                if (newSymbols[frameToEdit - 1][0] == "X") {
+                    if (pins == 0) {
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "-"
+                        setSymbols(newSymbols)
+                    } else if (pins == 10) {
+                        newSymbols[frameToEdit - 1] = "XX"
+                        setSymbols(newSymbols)
+                    } else {
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + pins + ""
+                        setSymbols(newSymbols)
+                    }
+                } else {
+                    let currentFrameValue = 0
+
+                    // The character could be a - which means 0 pins
+                    if (newSymbols[frameToEdit - 1] == "-") {
+                        currentFrameValue = 0
+                    } else {
+                        currentFrameValue = parseInt(newSymbols[frameToEdit - 1])
+                    }
+
+                    // The second roll should add to currentFrameValue and keep the frame value less than or equal to 10
+                    // If the current frame value plus whatever is rolled is 10, it is a spare
+                    if (currentFrameValue + pins == 10) {
+                        // Add a spare to the frame
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "/"
+                        setSymbols(newSymbols)
+
+                        // Autoset to next frame
+                        if (frameToEdit != 10) {
+                            setFrameToEdit(frameToEdit + 1)
+                        }
+                    } else if (currentFrameValue + pins < 10) {
+                        // Add whatever number to the frame
+
+                        // If zero pins, -
+                        if (pins == 0) {
+                            newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "-"
+                            setSymbols(newSymbols)
+                        } else {
+                            newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + pins + ""
+                            setSymbols(newSymbols)
+                        }
+
+                        // Autoset to next frame
+                        if (frameToEdit != 10) {
+                            setFrameToEdit(frameToEdit + 1)
+                        }
+                    } else {
+                        // Do nothing, because it adds to more than 10
+                    }
+                }
+            } else if (newSymbols[frameToEdit - 1].length == 2) {
+                let lastTwoFrameValue = 0
+                if (newSymbols[frameToEdit - 1][0] == "X") {
+                    // The character could be a - which means 0 pins
+                    if (newSymbols[frameToEdit - 1][1] == "-") {
+                        lastTwoFrameValue = 0
+                    } else {
+                        lastTwoFrameValue = parseInt(newSymbols[frameToEdit - 1][1])
+                    }
+
+
+                    // The second roll should add to currentFrameValue and keep the frame value less than or equal to 10
+                    // If the current frame value plus whatever is rolled is 10, it is a spare
+                    if (lastTwoFrameValue + pins == 10) {
+                        // Add a spare to the frame
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "/"
+                        setSymbols(newSymbols)
+
+                        // Autoset to next frame
+                        if (frameToEdit != 10) {
+                            setFrameToEdit(frameToEdit + 1)
+                        }
+                    } else if (lastTwoFrameValue + pins < 10) {
+                        // Add whatever number to the frame
+
+                        // If zero pins, -
+                        if (pins == 0) {
+                            newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "-"
+                            setSymbols(newSymbols)
+                        } else {
+                            newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + pins + ""
+                            setSymbols(newSymbols)
+                        }
+
+                        // Autoset to next frame
+                        if (frameToEdit != 10) {
+                            setFrameToEdit(frameToEdit + 1)
+                        }
+                    } else {
+                        // Do nothing, because it adds to more than 10
+                    }
+                } else {
+                    if (pins == 0) {
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "-"
+                        setSymbols(newSymbols)
+                    } else if (pins == 10) {
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "X"
+                        setSymbols(newSymbols)
+                    } else {
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + pins + ""
+                        setSymbols(newSymbols)
+                    } 
+                }     
+            }
+
+
+
+
         } else {
-            console.log("Pins is not 10, it is " + pins)
+            // This condition deals with if the frame is complete
+            if (newSymbols[frameToEdit - 1].length == 2 || newSymbols[frameToEdit - 1] == "X") {
 
-            setFrameSymbol("" + pins)
+            // This condition deals with if the frame is incomplete
+            } else if (newSymbols[frameToEdit - 1].length == 1) {
+                let currentFrameValue = 0
 
-            // let currentSymbolsList = symbols
-            // let frameString = currentSymbolsList[frameToEdit - 1]
-            // frameString = "" + pins
-            // currentSymbolsList[frameToEdit - 1] = frameString
-            // setSymbols(currentSymbolsList)
+                // The character could be a - which means 0 pins
+                if (newSymbols[frameToEdit - 1] == "-") {
+                    currentFrameValue = 0
+                } else {
+                    currentFrameValue = parseInt(newSymbols[frameToEdit - 1])
+                }
+
+                // The second roll should add to currentFrameValue and keep the frame value less than or equal to 10
+                // If the current frame value plus whatever is rolled is 10, it is a spare
+                if (currentFrameValue + pins == 10) {
+                    // Add a spare to the frame
+                    newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "/"
+                    setSymbols(newSymbols)
+
+                    // Autoset to next frame
+                    if (frameToEdit != 10) {
+                        setFrameToEdit(frameToEdit + 1)
+                    }
+                } else if (currentFrameValue + pins < 10) {
+                    // Add whatever number to the frame
+
+                    // If zero pins, -
+                    if (pins == 0) {
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + "-"
+                        setSymbols(newSymbols)
+                    } else {
+                        newSymbols[frameToEdit - 1] = newSymbols[frameToEdit - 1] + pins + ""
+                        setSymbols(newSymbols)
+                    }
+
+                    // Autoset to next frame
+                    if (frameToEdit != 10) {
+                        setFrameToEdit(frameToEdit + 1)
+                    }
+                } else {
+                    // Do nothing, because it adds to more than 10
+                }
+
+            } else { // The frame is currently empty
+                if (pins == 10) {
+                    // Add a strike and go to the next frame
+                    newSymbols[frameToEdit - 1] = "X"
+                    setSymbols(newSymbols)
+                    
+                    if (frameToEdit != 10) {
+                        setFrameToEdit(frameToEdit + 1)
+                    }
+                } else if (pins == 0) {
+                    // Add a -
+                    newSymbols[frameToEdit - 1] = "-"
+                    setSymbols(newSymbols)
+                } else {
+                    newSymbols[frameToEdit - 1] = pins + ""
+                    setSymbols(newSymbols)
+                }
+            }
         }
+        
+
     }
 
     let initialFrameSymbol = symbols[frameToEdit - 1]
-    console.log("Initial frame symbol: " + initialFrameSymbol)
+    let leftArrow = "<"
+    let rightArrow = ">"
 
     return (
         <LinearGradient
@@ -129,18 +335,14 @@ export default function App() {
                     />
 
                     <View style={styles.enterScoresController}>
+                        <Text style={styles.controlLabel}>Frame to Edit</Text>
                         <View style={styles.enterScoresControllerLeft}>
-                            <Text style={styles.controlLabel}>Frame to Edit</Text>
 
-                            <TextInput
-                                style={[styles.frameInput, { borderColor: frameInputBorderColor }]}
-                                onChangeText={handleFrameInputChange}
-                                keyboardType="numeric"
-                            >{frameToEdit}</TextInput>
-                            <View style={styles.frameReader}>
-                                {console.log(initialFrameSymbol)}
-                                <Text style={styles.frameReaderText}>{initialFrameSymbol}</Text>
+                            <RoundButton onPress={handleLeftArrow} text="<" borderRadius={0} width={50} marginTop={10} shadowOpacity={0.0}></RoundButton>
+                            <View style={styles.frameInput}>
+                                <Text style={styles.frameReaderText}>{frameToEdit}</Text>
                             </View>
+                            <RoundButton onPress={handleRightArrow} text=">" borderRadius={0} width={50} marginTop={10} shadowOpacity={0.0}></RoundButton>
 
                         </View>
                     </View>
@@ -193,17 +395,20 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         width: width * 0.9,
         borderRadius: 10,
-        alignItems: 'center'
+        alignItems: 'center',
     },
     enterScoresController: {
-        marginBottom: 40,
         width: 360,
-        height: 100,
-        flexDirection:'row'
+        marginBottom: 20,
+        flexDirection:'column',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     enterScoresControllerLeft: {
         width: 360,
-        height: 100,
+        flexDirection:'row',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     frameInput: {
         marginTop: 5,
@@ -212,13 +417,15 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         alignSelf: 'center',
         textAlign: 'center',
-        fontSize: 20
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    frameReaderContainer: {
+        marginBottom: 20
     },
     frameReader: {
         width:50,
         height:50,
-        borderWidth: 1,
-        borderTopWidth: 0,
         alignSelf: 'center',
         textAlign: 'center',
         fontSize: 20,
@@ -231,6 +438,7 @@ const styles = StyleSheet.create({
     },
     enterScoresControllerButtons: {
         width: 360,
+        marginTop: 10,
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection:'row',
